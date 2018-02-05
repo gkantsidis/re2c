0a0540db13e39caf687fcf68b462e5904621f564
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2018-02-05 21:08:35 +0000

    simple script to run some test cases

--- Win32/Test.ps1
+++ Win32/Test.ps1
@@ -1,3 +1,5 @@
+#Requires -Module psake
+
 [CmdletBinding()]
 param(
     [ValidateSet("Bootstrap", "Final")]
@@ -15,31 +17,59 @@ if ($Version -eq "Bootstrap") {
     $re2c = "re2c.exe"
 }
 
+function Assert-Continue {
+    param(
+        [bool]$Condition,
+        [string]$Message
+    )
+
+    if (-not $Condition) {
+        Write-Warning -Message "Error: $Message"
+    }
+}
+
 $path_to_executables = Join-Path -Path $PSScriptRoot -ChildPath "re2c"  | `
                        Join-Path -ChildPath $Architecture | `
                        Join-Path -ChildPath $Release
 
-if (-not (Test-Path -Path $path_to_executables -PathType Container)) {
-    Write-Error -Message "Cannot find directory '$path_to_executables'"
-    return
-}
+Assert (Test-Path -Path $path_to_executables -PathType Container) -failureMessage "Cannot find output folder in '$path_to_executables'"
+
+$re2c = Join-Path -Path $path_to_executables -ChildPath $re2c
+Assert (Test-Path -Path $re2c -PathType Leaf) -failureMessage "Cannot find re2c executable in '$re2c'"
 
 $test_executables = @(
     "range.exe"
     "s_to_n32_unsafe.exe"
 )
 
+# First, check that the simple test applications return the expected result
 foreach($executable in $test_executables) {
     $full_path = Join-Path -Path $path_to_executables -ChildPath $executable
     if (-not (Test-Path $full_path -PathType Leaf)) {
         Write-Warning -Message "Cannot find test executable '$executable' in '$full_path'"
     } else {
         & $full_path
-        $lec = $LASTEXITCODE
-        if ($lec -eq 0) {
-            Write-Verbose -Message "Test executable '$executable': OK"
-        } else {
-            Write-Error -Message "Test executable '$executable' returned error: $lec"
-        }
+        Assert-Continue -Condition ($LASTEXITCODE -Eq 0) -Message "Error code for '$executable' was expected to be 0, got $LASTEXITCODE"
     }
+}
+
+# Second, go through the examples directory
+$examples_directory = Join-Path -Path $PSScriptRoot -ChildPath .. | `
+                      Join-Path -ChildPath 're2c' | `
+                      Join-Path -ChildPath 'examples'
+$examples = Get-ChildItem -Path $examples_directory -Filter *.re
+foreach($example in $examples) {
+    Write-Verbose -Message "Examining '$($example.BaseName)'"
+
+    [string[]]$arguments = @()
+
+    if ($example.BaseName.Contains('--tags')) {
+        $arguments += "-T"
+    }
+    if ($example.BaseName.Contains('cr8i')) {
+        $arguments += @("-r", "-c")
+    }
+
+    [string]$result = & $re2c @arguments $example.FullName
+    Assert-Continue -Condition ([string]::IsNullOrWhiteSpace($result) -eq $false) -Message "Got empty output for '$example'"
 }
